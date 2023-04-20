20aa513505bb640eb5e2a23e15c0d2d21903ea8b
sv101 <70589012+sv101@users.noreply.github.com>
2023-04-20 14:38:56 -0400

    Update GlobalHeader.js

--- src/Components/GlobalHeader.js
+++ src/Components/GlobalHeader.js
@@ -7,7 +7,7 @@ import { Button } from '@mui/material';
 const GlobalHeader = (props) => {
     return (
       <AppBar position="static" style={{backgroundColor:'rgb(23,174,117)'}}>
-      <Toolbar style={{ padding: '10px 35px'}}>
+      <Toolbar style={{ padding: '11px 35px'}}>
         <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
         <b>Sustainability on the GO!</b>
         </Typography>
